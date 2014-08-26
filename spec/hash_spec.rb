cd0c925b2539db6e7c2947339df9db5b9e6b2f46
Roi Driscoll <roi.driscoll@gmail.com>
2014-08-26 23:55:12 +0100

    Add hash flatten test

--- spec/hash_spec.rb
+++ spec/hash_spec.rb
@@ -1,8 +1,9 @@
 describe 'Hash' do
 
+  let(:smash) { {a: "a", b: "b"} }
+
   context 'DELETE:' do
     it 'Deletes key value pair' do
-      smash = {a: "a"}
       smash.delete(:a)
       expect(smash).to_not have_key(:a)
     end
@@ -11,19 +12,16 @@ describe 'Hash' do
   context 'EACH:' do
     it 'Calls a block of code, using a choice of key and/or value as arguments' do
       def hashing_each
-        mash = {a: "a", b: "b"}
-        mash.each{|element| puts "#{element}"}
+        smash.each{|element| puts "#{element}"}
       end
       expect(hashing_each).to include :a => "a"
     end
   end
 
-  # context 'EACH_KEY:' do
-  #   it 'Deletes key value pair' do
-  #     smash = {a: "a"}
-  #     smash.each_key {|key| mouth = key}
-  #     expect(smash).to_not have_key(:a)
-  #   end
-  # end
+  context 'FLATTEN:' do
+    it 'Returns a one-dimensional array which is a flattened version of the source hash ' do
+      expect(smash.flatten).to eq [:a,"a",:b,"b"]
+    end
+  end
 
 end
